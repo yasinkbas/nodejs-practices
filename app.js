d06eeb8d6d6afbf47557fb1cc7936ae3ec6cb772
yasinkbas <yasin.kbas12@gmail.com>
2018-12-31 21:38:30 +0300

    mongodb is connected

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// Modules
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
+// Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// App
 var app = express();
+
+// mongodb://host/dbname
+mongoose.connect('mongodb://localhost/udemy', { useNewUrlParser: true})
+    .then(() => {
+        console.log('Mongodb is connected');
+    })
+    .catch((err) => {
+        console.log('mongodb error :',err)
+    })
 
 app.use(logger('dev'));
 app.use(express.json());
